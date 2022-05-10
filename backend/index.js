6e61bb5b17c3552f9fbbb1ec58d3ad8d6789e90f
Felzj <jeffychawin@gmail.com>
2022-05-10 17:33:50 +0700

    [Update] Finish Frontend Backend, Automate Testing

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 8000
 
+const User = require('./src/model/user');
+
 const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI)
 .then(() => {
@@ -28,12 +30,18 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', async (req, res) => {
-    // await User.find({}, function (err, users) {
-    //     var data = []
-    //     users.forEach(function (user) {
-    //         data.push(user.email);
-    //     });
-    //     res.send(data)
-    // }).clone().catch(function (err) { console.log("getAllUserEmail Error : " + e); })
     res.send("Test")
+})
+
+app.get('/user', async (req, res) => {
+    await User.find({}, function (err, users) {
+        res.send(users)
+    }).clone().catch(function (err) { console.log("getUser Error : " + e); })
+})
+
+app.post('/user', async (req, res) => {
+    const user = new User(req.data)
+    await user.save((err, doc) => {
+        res.send(doc)
+    })
 })
